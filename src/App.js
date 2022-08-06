cce9fdb6070f7e56ddf1d8181537e6609bc774b5
SanjanaSinghh <sanjana.kolkata13@gmail.com>
2022-08-06 16:08:35 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
+ import Login from './component/login';
+import Home from './component/home';
+import Products from './component/products';
+import Nav from './component/navbar';
+import Cart from './component/cart';
+
+
+import { Routes,Route } from 'react-router-dom';
+import { useSelector,useDispatch } from 'react-redux'
+import{additem,deleteitem} from './features/cart'
 function App() {
+  const cart=useSelector((s)=>s.Cart.value)
+  console.log(additem,deleteitem)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Routes>
+<Route path='/' element={<Nav />}>
+<Route index element={ <Home />} />
+<Route path='/login' element={<Login />}/>
+<Route path='/cart' element={<Cart />}/>
+<Route path='/product/:id' element={<Products />}/>
+  
+
+</Route>
+
+
+    </Routes>
+    
+    </>
   );
 }
 
